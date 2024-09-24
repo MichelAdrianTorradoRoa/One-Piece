f260db649332854b2da34e873f1a914389774b39
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2024-09-24 17:03:21 -0500

    feat: :art: Creacion bosquejo de las tablas
    by @MichelAdrianTorradoRoa

---
+++ ddl.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Piratas(
+    Id_Pirata INT NOT NULL PRIMARY KEY,
+
+)
+
+CREATE TABLE Rango_Pirata(
+    Id_RangoP INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Tripulaciones(
+    Id_Tripulacion INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Navios(
+    Id_Navio INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Gobierno_Mundial(
+    Id_Gobierno INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Islas(
+    Id_Isla INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Marinos(
+    Id_Marino INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Rango_Marinos(
+    Id_RangoM INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Division_Marinos(
+    Id_Division INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Reinos(
+    Id_Reino INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Frutas_del_Diablo(
+    Id_FrutaDiablo INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Armamentos(
+    Id_Armamento INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE Batallas(
+    Id_Batalla INT NOT NULL PRIMARY KEY,
+)
+
+CREATE TABLE
+
+
